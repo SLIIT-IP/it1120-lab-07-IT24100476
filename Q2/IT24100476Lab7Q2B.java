8dd01a1905cf976f0a42ff4cddf6f1874862e9d1
IT24100476 <it24100476@my.sliit.lk>
2024-10-02 04:31:41 +0530

    IT24100476Lab7Q2B

---
+++ Q2/IT24100476Lab7Q2B.java
@@ -0,0 +1,27 @@
+public class IT24100476Lab7Q2B{
+public static void main(String[] args){
+
+
+int i;
+int j;
+int k =1;
+
+
+
+for(i=0; i<=4; i++){
+  System.out.print(k + "  - ");
+  k++;
+
+     for(j=0; j<=i; j++){
+        System.out.print(" * ");
+
+       } 
+  System.out.println("  ");
+
+}
+
+
+
+
+  }
+}

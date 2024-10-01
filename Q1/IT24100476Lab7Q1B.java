67ff2cf3bd897b4bd399c211676f41407cfe6c0d
IT24100476 <it24100476@my.sliit.lk>
2024-10-02 04:30:02 +0530

    IT24100476Lab7Q1B

---
+++ Q1/IT24100476Lab7Q1B.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class IT24100476Lab7Q1B{
+public static void main(String[] args){
+
+Scanner input = new Scanner(System.in);
+
+int x=1;
+int i;
+
+int mark[] = new int[4];
+
+for(i = 0; i<mark.length;  i++){
+
+//Get user inputs
+System.out.print("Enter Subject Mark "+x+" : " );
+mark[0]  = input.nextInt();
+mark[1]  = input.nextInt();
+mark[2]  = input.nextInt();
+mark[3]  = input.nextInt();
+
+
+x++;
+
+}
+
+//Create a totalMarks array
+double totalMarks = (double)mark[0] +(double)mark[1] +(double)mark[2] +(double)mark[3] ;
+
+//Calculating average
+double average = totalMarks /4;
+
+System.out.println(" " );
+System.out.println("Average is : "+average );
+
+if(average<=100 && average>=0){
+      if(average>=75){
+     
+      System.out.print("overall Grade is : Distinction " );
+
+          }
+    else if(average>=50){
+     
+      System.out.print("overall Grade is : Credit " );
+
+          }
+  else  if(average>=0){
+     
+      System.out.print("overall Grade is : Fail" );
+
+          }
+
+
+}//outer if
+
+  }
+}
